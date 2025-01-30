b89aa9ec18e419c8e7b2c84f16c71ee6f0cd6c16
Emory Russell <emorywrussell@gmail.com>
2025-01-29 18:20:29 -0600

    u

--- logging_server.h
+++ logging_server.h
@@ -7,25 +7,22 @@
 class LoggingServer : public QObject {
     Q_OBJECT
     public:
-    QTcpServer *server;
+    QTcpServer server;
     LoggingServer() {
-        server = new QTcpServer();
-        if (server->listen(QHostAddress::LocalHost, 5002)) {
-            std::cout << "listening\n";
+        int port = 5005;
+        if (server.listen(QHostAddress::Any, port)) {
+            std::cout << "listening on port ";
+            std::cout << port;
+            std::cout << "\n";
         } else {
             std::cout << "listen failed\n";
         }
-        connect(server, &QTcpServer::newConnection, this, &LoggingServer::new_connection);
+        connect(&server, &QTcpServer::newConnection, this, &LoggingServer::new_connection);
     }
 
-    void shutdown() {
-        server->close();
-        delete server;
-        QCoreApplication::quit();
-    }
     private slots:
     void new_connection() {
-        QTcpSocket *socket = server->nextPendingConnection();
+        QTcpSocket *socket = server.nextPendingConnection();
         std::cout << "Connection from: " + socket->peerAddress().toString().toStdString() + "\n";
         connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
         connect(socket, &QTcpSocket::readyRead, this, &LoggingServer::read_ready);
@@ -35,3 +32,4 @@ class LoggingServer : public QObject {
         std::cout << socket->readAll().toStdString();
     }
 };
+
